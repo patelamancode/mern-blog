43db906cae93b9a38a7600258244a99eec9d1f6f
Aman <patelamancoder@gmail.com>
2024-03-26 17:15:11 +0530

    create: Header section with responsive for all  sizes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Header from "./components/Header"
+import Home  from "./pages/Home"
+import About from "./pages/About"
+import Dashboard from "./pages/Dashboard"
+import Projects from "./pages/Projects"
+import SignUp from "./pages/SignUp"
+import SignIn from "./pages/SignIn"
+
 function App() {
 
   return (
-    <>
-      <div className="text-3xl text-red-500">Hello my App</div>
-    </>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/about" element={<About />}/>
+        <Route path="/dashboard" element={<Dashboard />}/>
+        <Route path="/projects" element={<Projects />}/>
+        <Route path="/signup" element={<SignUp />}/>
+        <Route path="/signin" element={<SignIn />}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
