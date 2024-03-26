d2dde231a4a9875aad1e7e799f44da4acaf8fcd1
Aman <patelamancoder@gmail.com>
2024-03-26 20:25:35 +0530

    connecting mongoDB with the backend server

--- api/index.js
+++ api/index.js
@@ -1,6 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_STR)
+  .then(() => {
+    console.log("Database is connected");
+  })
+  .catch((err) => console.log(err));
 
 app.listen(3000, () => {
   console.log(`server is running on port 3000 !!!`);
