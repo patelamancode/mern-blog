2a9520ba5ae31868a1292758f55cc7c11a20c0a5
Aman <patelamancoder@gmail.com>
2024-03-30 05:28:21 +0530

    adding redux-persist  module o store  session storage for  user info

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,14 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import {store} from "./redux/store.js"
-import { Provider } from 'react-redux';
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { store, persist } from "./redux/store.js";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <PersistGate loading={null} persistor={persist}>
   <Provider store={store}>
-    <App />
-  </Provider>,
-)
+      <App />
+  </Provider>
+  </PersistGate>
+);
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,24 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import userSlice from "./slices/userSlice";
 
-export const store = configureStore({
-  reducer: {
-    user: userSlice,
-  },
+const rootReducer = combineReducers({
+  user: userSlice,
 });
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+export const persist = persistStore(store);
